0b6903e37a2e0ee87de3cf10a41378246d8dcb46
Mahdi Naderian <naderian.mahdi@gmail.com>
2022-03-11 14:20:25 +0330

    add temperature conversion

---
+++ tests/TemperatureTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mnaderian\UnitConversions\Tests;
+
+use Mnaderian\UnitConversions\Temperature;
+use PHPUnit\Framework\TestCase;
+
+class TemperatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::forCelsius(100)->toFahrenheit();
+
+        $this->assertEquals(212, $fahrenheit);
+    }
+}
